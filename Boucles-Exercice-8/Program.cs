3228c4c9d175067615354b6e0007cda1d06b1c9f
Sulyvan Debraize <sulyvan.debraize@net-c.com>
2018-12-18 16:28:12 +0100

    Exercice completé

---
+++ Boucles-Exercice-8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucles_Exercice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
